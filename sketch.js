971693bb0d981a7cd50cea09fdbdc7623ff3559f
Rashid Al-Abri <rashid@DN51udid.SUNet>
2021-11-30 03:17:32 -0800

    updates

--- sketch.js
+++ sketch.js
@@ -5,15 +5,17 @@ let j;
 
 let gloria;
 
+let increasing;
+
 function preload() {
-  gloria = loadFont('assets/WorkSans-Bold.ttf');
+  gloria = loadFont("assets/WorkSans-Bold.ttf");
 }
 
 let bg;
 
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2, WEBGL);
-  
+  createCanvas(windowWidth, windowHeight);
+
   textFont(gloria);
   textSize(width / 30);
   textStyle(BOLD);
@@ -21,38 +23,43 @@ function setup() {
 
   i = 0;
   j = 0;
-    
-  for(let k = 0; k < min(width, 1000); k++){
+
+  for (let k = 0; k < width / 10; k++) {
     values.push(random(360));
   }
-    
+
   frameRate(25);
   colorMode(HSB);
+
+  increasing = true;
 
   bg = 0;
 }
 
 function draw() {
-  background(color(values[-1], 200, 200));
+  //   background(color(bg, 200, 200));
+  background(0);
 
   bubbleSort();
+
   simulateBubbleSort();
 
   fill(0);
-  strokeWeight(0);
-  let time = millis();
-  rotateY(0.25* sin(0.001 * time));
-  rotateZ(0.25 * cos(0.001 * time));
-  rotateX(0.1 * tan(0.0001 * time));
+  strokeWeight(4);
+  stroke(color(bg, 200, 100));
+  textSize(width / 30 + 0.1 * (width / 30) * sin(0.0025 * millis()));
 
-  text('Bubble is my name.\nPushing you down is my game,\nand to sort, I came.', 0, 0);
+  text(
+    "Bubble is my name.\nPushing you down is my game,\nand to sort, I came.",
+    width / 2,
+    height / 2
+  );
 
   if (bg < 360) {
-      bg += 1;
+    bg += 1;
   } else {
-      bg = 0;
+    bg = 0;
   }
-
 }
 
 function drawGradient(x, y, w, h, c1, c2) {
@@ -62,7 +69,7 @@ function drawGradient(x, y, w, h, c1, c2) {
     let c = lerpColor(c1, c2, inter);
     stroke(c);
     strokeWeight(1);
-    line(i , y, -100, i, y + h, -100);
+    line(i, y, i, y + h);
   }
 }
 
@@ -72,36 +79,31 @@ function simulateBubbleSort() {
     x = k * w;
     c1 = color(values[k], 200, 200);
     c2 = color(values[k + 1], 200, 200);
-    drawGradient(x - int(width / 2), -int(height / 2), w, height, c1, c2);
+    drawGradient(x, 0, w, height, c1, c2);
   }
 }
 
 function bubbleSort() {
-  for(let k = 0; k < 250; k++){
-    if(i < values.length) {
+  for (let k = 0; k < 250; k++) {
+    if (i < values.length) {
       let temp = values[j];
-      if(values[j] > values[j + 1]) {
-        values[j] = values[j+1];
-        values[j+1] = temp;
+      if (values[j] > values[j + 1]) {
+        values[j] = values[j + 1];
+        values[j + 1] = temp;
       }
       j++;
-      
-      if(j>=values.length-i-1){
+
+      if (j >= values.length - i - 1) {
         j = 0;
         i++;
       }
-    }
-    else{
-        values = [];
-        i = 0;
-        j = 0;
-        for(let i = 0;i<width/1;i++){
-            values.push(random(360));
-        }
+    } else {
+      values = [];
+      i = 0;
+      j = 0;
+      for (let k = 0; k < width / 10; k++) {
+        values.push(random(360));
+      }
     }
   }
 }
-
-// Bubble is my name,
-// pushing you down is my game
-// and to sort I came
